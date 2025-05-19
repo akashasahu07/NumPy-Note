db18aea40ead7270d15888807ef6012712b9f1d6
Akasha Sahu <akashasahu2001@gmail.com>
2025-05-19 21:55:19 +0530

    button added

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 // Scroll to Top Function code
 
 function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
 
 // Dark mode code
 window.onload = function () {
-    const btnIcon = document.querySelector("#darkModeBtn i");
-    const isDark = localStorage.getItem("dark-mode") === "enabled";
+  const btnIcon = document.querySelector("#darkModeBtn i");
+  const isDark = localStorage.getItem("dark-mode") === "enabled";
 
-    if (isDark) {
-      document.body.classList.add("dark-mode");
-      btnIcon.classList.remove("fa-moon");
-      btnIcon.classList.add("fa-sun");
-    } else {
-      btnIcon.classList.remove("fa-sun");
-      btnIcon.classList.add("fa-moon");
-    }
-  };
-
-  function toggleDarkMode() {
-    const btnIcon = document.querySelector("#darkModeBtn i");
-    const isNowDark = document.body.classList.toggle("dark-mode");
-
-    if (isNowDark) {
-      btnIcon.classList.remove("fa-moon");
-      btnIcon.classList.add("fa-sun");
-      localStorage.setItem("dark-mode", "enabled");
-    } else {
-      btnIcon.classList.remove("fa-sun");
-      btnIcon.classList.add("fa-moon");
-      localStorage.setItem("dark-mode", "disabled");
-    }
+  if (isDark) {
+    document.body.classList.add("dark-mode");
+    btnIcon.classList.remove("fa-moon");
+    btnIcon.classList.add("fa-sun");
+  } else {
+    btnIcon.classList.remove("fa-sun");
+    btnIcon.classList.add("fa-moon");
   }
+};
+
+function toggleDarkMode() {
+  const btnIcon = document.querySelector("#darkModeBtn i");
+  const isNowDark = document.body.classList.toggle("dark-mode");
+
+  if (isNowDark) {
+    btnIcon.classList.remove("fa-moon");
+    btnIcon.classList.add("fa-sun");
+    localStorage.setItem("dark-mode", "enabled");
+  } else {
+    btnIcon.classList.remove("fa-sun");
+    btnIcon.classList.add("fa-moon");
+    localStorage.setItem("dark-mode", "disabled");
+  }
+}
